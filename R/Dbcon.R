f91f87df676cfc01cf9e62266360020a2073a2d5
petermeissner <retep.meissner@gmail.com>
2018-08-20 19:47:23 +0200

    init

---
+++ R/Dbcon.R
@@ -0,0 +1,74 @@
+#' Class providing object with methods for communication with lightning-viz server
+#'
+#' @docType class
+#'
+#' @importFrom R6 R6Class
+#'
+#' @export
+#'
+#' @keywords data
+#'
+#' @return Object of \code{\link{R6Class}} with methods for communication with lightning-viz server.
+#'
+#' @format \code{\link{R6Class}} object.
+#'
+#'
+#' @examples
+#' @field con
+#'
+#' @section Methods:
+#'
+#' \describe{
+#'   \item{\code{gallery(imgpathvector)}}{This method uploads images from vector of file paths \code{imgpathvector} to the server and creates a gallery of these images.}
+#'   }
+
+
+Dbcon <-
+  R6::R6Class(
+    classname = "dbcon",
+    public =
+      list(
+        con = NULL,
+        initialize =
+          function(
+            con  = NULL,
+            ...
+          ){
+
+            # handle connection information
+            # ... or set up connection
+            # ... or use ANSI as default
+            if ( is.null(con) & length(list(...)) == 0 ){
+              # DEV : # con <- DBI::ANSI()
+              con <- DBI::dbConnect(RSQLite::SQLite(), dbname =":memory:")
+            }else{
+              con <- DBI::dbConnect(...)
+            }
+
+            # set connection field
+            self$con <- con
+          }
+      )
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
